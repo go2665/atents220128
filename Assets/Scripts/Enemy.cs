a35eb83317bab64a25e3f6b049b2420228a5ab34
go2665 <77224762+go2665@users.noreply.github.com>
2022-02-24 15:09:45 +0900

    백그라운드 스크롤링 완성
    적 만들기 시작
    프리팹 추가
    애니메이션 오버라이드 컨트롤러 추가
    적 스포너 과제

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //이 게임 오브젝트는 게임이 시작되면 -x축 방향으로 일정한 속도로 이동한다.
+    //이동하는 속도는 moveSpeed라는 변수에 저장되어 있다.
+    //12:35분까지 실습 진행(쉬는시간 포함)
+
+    //힌트
+    //transform에 position에 위치값이 저장되어 있음
+    //transform에 어떤 함수가 있음
+
+    public float moveSpeed = 1.5f;
+
+    //private void Start()
+    //{
+    //    //this.transform.position;    //위치 (0,0,0)
+    //    //this.transform.rotation;    //회전 (0,0,0)
+    //    //this.transform.localScale;  //크기배율 (1,1,1)
+
+    //    // (0,1,1)+(1,3,1) = (1,4,2)
+    //    // (1,2,3) * 2 = (2,4,6)
+    //}
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);        
+    }
+
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private void Start()
+    {
+        // Instantiate : 프리팹을 게임 오브젝트로 동적 생성하는 함수
+        // Destroy : 게임 오브젝트를 삭제하는 함수
+        // Random.Range : 랜덤한 숫자를 돌려주는 함수
+
+        // 위 함수들을 사용하여 게임 화면 오른쪽 끝에서 랜덤한 높이로 적 새가 생성되는 코드를 작성하시오
+
+    }
+}
