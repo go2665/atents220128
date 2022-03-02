cd966e3525254d83174abc8703742ff0e8686e01
go2665 <77224762+go2665@users.noreply.github.com>
2022-03-02 12:05:57 +0900

    비트플래그
    비트플래스를 이용해서 스폰될 장소 결정
    GetFlags함수

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -45,48 +45,98 @@ public class EnemySpawner : MonoBehaviour
         
         // 그 후 반복해서 생성 시작
         while (true)    // 무한 루프
-        {            
-            bool result = false;    // 최소 하나 이상의 새를 생성할 수 있는지 확인하는 플래그
-            bool[] flags = new bool[MAX_SPACE_COUNT];   // 새가 생성될 칸이 true로 표시된 배열
-            while (result == false) // 만약 하나 이상의 새를 생성할 수 없는 경우 다시 계산
-            {
-                // flags에 랜덤으로 값 입력. true면 해당칸의 새를 생성하고 false면 생성하지 않는다.
-                for (int i = 0; i < MAX_SPACE_COUNT; i++)
-                {
-                    if (Random.Range(0, 2) == 1)
-                        flags[i] = true;
-                }
-
-                // 무조건 비워질 칸을 결정
-                // flags에 설정된 값과 상관 없이 비워질 공간을 지정해 덮어 쓴다.
-                int index = Random.Range(0, MAX_SPACE_COUNT - 1);
-                flags[index] = false;       //랜덤으로 한칸을 무조건 비운다.
-                flags[index + 1] = false;   //그 다음 칸도 비워 2개 연속된 빈 공간이 생기게 만든다.
-
-                // 새가 하나 이상 생성되는지 확인
-                for( int i=0;i<MAX_SPACE_COUNT;i++)
-                {
-                    // 한줄에 새가 전부 안나오는 버그 수정을 위해 flags모두 검사
-                    if (flags[i] == true)
-                    {
-                        // 하나라도 true가 나왔다는 것은 빈칸이 아닌 곳이 최소 1개는 있다는 의미                        
-                        result = true;      // 51번 라인의 while을 벗어나기 위해 변수 변경
-                        break;              // 67번 라인의 for를 벗어나기 위해 break;
-                    }    
-                }
-            }
+        {
+            // bool[] flags = GetFlagsBoolType();
+            int flags = GetFlags();
 
             // flags에 설정된 값에 따라 새 생성
-            for( int i=0; i<MAX_SPACE_COUNT; i++)
+            for (int i = 0; i < MAX_SPACE_COUNT; i++)
             {
-                if(flags[i] == true)
+                //if (flags[i] == true) // GetFlagsBoolType용 조건문
+
+                //새로운 if를 완성해보기
+                //if()
                 {
                     EnemyGenerate(i);
                 }
-            }            
+            }
 
             yield return new WaitForSeconds(spawnInterval);
         }
+    }
+
+    private int GetFlags()
+    {
+        // 리턴용 변수. 최종적으로 계산한 플래그 값이 들어갈 변수.
+        // 6비트만 남겨 놓을 것임. 그 윗부분은 무조건 0
+        // 1로 설정된 칸에서는 적 새가 생성되고
+        // 0으로 설정된 칸에서는 아무것도 생성되지 않는다.
+        int flags = 0;          
+
+        while (flags == 0)  // 모든 칸이 비는 것을 방지하기 위해서 설정
+        {
+            int random = (int)(Random.value * 10000.0f);  //화이트보드 1번. 6비트에 각각 비트 세팅
+
+            // (1 << MAX_SPACE_COUNT) - 1 결과로 0000 0000 0000 0000 0000 0000 0011 1111 생성
+            //  1 == 0b_0000_0001
+            //  MAX_SPACE_COUNT만큼 왼쪽으로 쉬프트 == 0b_0100_0000
+            //  결과-1 ==  0b_0011_1111
+            random &= ((1 << MAX_SPACE_COUNT) - 1); //random = random & ((1 << MAX_SPACE_COUNT) - 1);
+
+            //결과 예시
+            //   0101 0101 0101 0101 0101 0101 0101 0101    (random 변수의 값으로 가정)
+            // & 0000 0000 0000 0000 0000 0000 0011 1111    (랜덤으로 나온값을 6bit남기는 방법, (1 << MAX_SPACE_COUNT) - 1의 결과)
+            //   0000 0000 0000 0000 0000 0000 0001 0101
+
+            int mask = 0b_0011;
+            mask = mask << Random.Range(0, MAX_SPACE_COUNT - 1);    //mask를 랜덤하게 쉬프트(아래 5개 중 하나가 되게 설정)
+            //11 0000
+            //01 1000
+            //00 1100
+            //00 0110
+            //00 0011
+            mask = ~mask;   //not연산을 통해 bit값 뒤집기
+                            //00 1100 --> 1111 1111 1111 1111 1111 1111 1111 0011
+
+            flags = random & mask;  //최종적으로 random값에 mask값을 & 시켜서 두칸 비우기
+        } 
+
+        return flags;
+    }
+
+    private bool[] GetFlagsBoolType()
+    {
+        bool result = false;    // 최소 하나 이상의 새를 생성할 수 있는지 확인하는 플래그
+        bool[] flags = new bool[MAX_SPACE_COUNT];   // 새가 생성될 칸이 true로 표시된 배열
+        while (result == false) // 만약 하나 이상의 새를 생성할 수 없는 경우 다시 계산
+        {
+            // flags에 랜덤으로 값 입력. true면 해당칸의 새를 생성하고 false면 생성하지 않는다.
+            for (int i = 0; i < MAX_SPACE_COUNT; i++)
+            {
+                if (Random.Range(0, 2) == 1)
+                    flags[i] = true;
+            }
+
+            // 무조건 비워질 칸을 결정
+            // flags에 설정된 값과 상관 없이 비워질 공간을 지정해 덮어 쓴다.
+            int index = Random.Range(0, MAX_SPACE_COUNT - 1);
+            flags[index] = false;       //랜덤으로 한칸을 무조건 비운다.
+            flags[index + 1] = false;   //그 다음 칸도 비워 2개 연속된 빈 공간이 생기게 만든다.
+
+            // 새가 하나 이상 생성되는지 확인
+            for (int i = 0; i < MAX_SPACE_COUNT; i++)
+            {
+                // 한줄에 새가 전부 안나오는 버그 수정을 위해 flags모두 검사
+                if (flags[i] == true)
+                {
+                    // 하나라도 true가 나왔다는 것은 빈칸이 아닌 곳이 최소 1개는 있다는 의미                        
+                    result = true;      // 51번 라인의 while을 벗어나기 위해 변수 변경
+                    break;              // 67번 라인의 for를 벗어나기 위해 break;
+                }
+            }
+        }
+
+        return flags;
     }
 
     // 적을 생생하는 함수
@@ -104,5 +154,6 @@ public class EnemySpawner : MonoBehaviour
     //2월 28일 과제
     // 코드가 이해되지 않은 사람 -> 주석보고 코드 이해해서 오기
     // 코드가 이해된 사람 -> bit 연산을 이용해 flags를 세팅하고 사용하도록 코드를 작성하기
+
 
 }
--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -3,25 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test2 : MonoBehaviour
-{
-    IEnumerator YieldTesT()
-    {
-        Debug.Log(1);
-        yield return null;
-        Debug.Log(2);
-        yield return null;
-        Debug.Log(3);
-        yield return null;
-        Debug.Log(4);
-    }
+{   
 
     private void Start()
     {
         Debug.Log("Test2");
-        YieldTesT();
-        YieldTesT();
-        YieldTesT();
-        YieldTesT();
-        YieldTesT();
+        Random.InitState(0);
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("random value : " + Random.value);
+        }
     }
 }
