a24cde29e6134b6832bbd28c15fa80a38a0ba719
go2665 <77224762+go2665@users.noreply.github.com>
2022-03-07 14:21:48 +0900

    Update Enemy.cs
    필요없는 주석 삭제

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -46,9 +46,6 @@ public class Enemy : MonoBehaviour
                     GameManager.Inst.Score += 1;    //static 클래스인 GameManager에 점수 +1 기록
                     getScore = true;    // 점수는 한번만 +1이 되도록 설정
                     Debug.Log($"현재 점수 : {GameManager.Inst.Score}");
-
-                    //Text scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
-                    //scoreText.text = $"Score : {GameManager.Inst.Score}";
                 }
             }
         }
